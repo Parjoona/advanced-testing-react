8e613da2ae8b8901a498dc867d08afdec40659b2
parjoona <parjoona@hotmail.com>
2019-01-30 21:33:55 +0100

    cleaned out unneccesaries from template

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from './Box/Box'
+import List from './List/List'
+
+const App = () => (
+  <div className="App">
+    Hello React
+    <List/>
+    <Box/>
+  </div>
+)
+
+export default App 
---
+++ src/Components/Box/Box.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Box = () => (
+  <div>
+    Box
+  </div>
+)
+
+export default Box
