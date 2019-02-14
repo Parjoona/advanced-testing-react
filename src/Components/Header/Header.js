144056b21f2f332b1e136bec6706ef0f747ce17b
parjoona <parjoona@hotmail.com>
2019-02-14 20:01:39 +0100

    created HOC for authentication

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,35 @@
-import React from 'react' 
+import React, { Component } from 'react' 
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import * as actions from 'Redux/Actions'
 
-const Header = (props) => (
-  <ul>
-    <li>
-      <Link to='/'>Home</Link>
-    </li>
-    <li>
-      <Link to='/post'>Post</Link>
-    </li>
-    <li>
-      <Link to="/comments">Comments</Link>
-    </li>
-    <li>
-      {props.auth ? (
-        <button>Sign out</button>
-      ) : (
-        <button>Sign in</button>
-      )}
-    </li>
-  </ul>
-)
+class Header extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post</Link>
+        </li>
+        <li>
+          <Link to="/comments">Comments</Link>
+        </li>
+        <li>
+          {this.props.auth ? (
+            <button onClick={() => this.props.changeAuth(false)}>Sign out</button>
+          ) : (
+            <button onClick={() => this.props.changeAuth(true)}>Sign in</button>
+          )}
+        </li>
+      </ul>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps, actions)(Header)
--- src/Router/RouterConfig.js
+++ src/Router/RouterConfig.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import App from 'Components/App';
@@ -6,18 +6,15 @@ import Box from 'Components/Box/Box'
 import List from 'Components/List/List'
 import Header from 'Components/Header/Header'
 
-export default class RouterConfig extends Component {
+const RouterConfig = () => (
+  <div>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/post" component={Box} />
+      <Route path="/comments" component={List} />
+    </Switch>
+  </div>
+)
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={App} />
-          <Route path="/post" component={Box} />
-          <Route path="/comments" component={List} />
-        </Switch>
-      </div>
-    )
-  }
-}
+export default RouterConfig
