3bfcb7bf80743dbbb80f467154c173c6c75d3fb9
parjoona <parjoona@hotmail.com>
2019-02-14 22:09:49 +0100

    added async middleware functionality

--- src/Middlewares/async.js
+++ src/Middlewares/async.js
@@ -1,3 +1,15 @@
 export default ({ dispatch }) => (next) => (action) => {
+  if (!action.payload || !action.payload.then) {
+    return next(action)
+  }
 
+  action.payload.then((response) => {
+    const payloadAction = {
+      ...action,
+      payload: response
+    }
+  
+    dispatch(payloadAction)
+
+  })
 }
